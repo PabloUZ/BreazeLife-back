d752393e2029c8bec705023c15764c66badacd16
DianaPaque <dianas.paquep@autonoma.edu.co>
2026-05-12 02:15:26 -0500

    feat(BLIFE-18): create employee registration DTOs

---
+++ src/main/java/com/highdev/breazelife/modules/employer/dto/request/RegisterEmployeeRequest.java
@@ -0,0 +1,64 @@
+package com.highdev.breazelife.modules.employer.dto.request;
+
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class RegisterEmployeeRequest {
+    @NotBlank
+    @Size(max = 30)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 30)
+    private String lastName;
+
+    @NotBlank
+    @Size(max = 255)
+    private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^[0-9]+$")
+    @Size(max = 11)
+    private String document;
+
+    @NotNull
+    @Past
+    private LocalDate birthDate;
+
+    @NotBlank
+    @Size(max = 50)
+    private String position;
+
+    @NotNull
+    @Positive
+    private BigDecimal baseSalary;
+
+    @NotNull
+    private LocalDate startDate;
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getDocument() { return document; }
+    public void setDocument(String document) { this.document = document; }
+
+    public LocalDate getBirthDate() { return birthDate; }
+    public void setBirthDate(LocalDate birthDate) { this.birthDate = birthDate; }
+
+    public String getPosition() { return position; }
+    public void setPosition(String position) { this.position = position; }
+
+    public BigDecimal getBaseSalary() { return baseSalary; }
+    public void setBaseSalary(BigDecimal baseSalary) { this.baseSalary = baseSalary; }
+
+    public LocalDate getStartDate() { return startDate; }
+    public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
+}
